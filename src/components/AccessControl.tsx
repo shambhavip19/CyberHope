5b6346e883e31180a6052940c27460f4e5b229ec
shambhavip19 <shambhavipahade@gmail.com>
2025-09-26 10:15:11 +0530

    Updated AccessControl.tsx

--- src/components/AccessControl.tsx
+++ src/components/AccessControl.tsx
@@ -180,7 +180,7 @@ export const AccessControl: React.FC = () => {
           onClick={loadUserEvidences}
           className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg transition-colors"
             loadAllAccessRequests();
-        
+        >
           Refresh
         </button>
       </div>
